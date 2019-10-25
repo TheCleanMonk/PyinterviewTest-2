dacba80da359fe072feab6c27dae11d3f5fd5d0e
Zach <drexel921@gmail.com>
2019-10-25 16:08:06 +0100

    last commit

--- quiz.py
+++ quiz.py
@@ -16,28 +16,32 @@ def longestWord(input):
 
 userInput = "Emoticons, irregular spellings and exclamation points in text messages aren't sloppy or a sign that written language is going down the tubes—these 'textisms' help convey meaning and intent in the absence of spoken conversation, according to newly published research from Binghamton University, State University of New York."
 longestWord(userInput)
-if 1 == 1:
-    print("eat shit")
 
 #Question 3
+
+sleepTime = 8
 def setup():
-    driver = webdriver.Chrome("C:/cygwin64/home/Zach Hitchens/pySeleniumTest/chromedriver.exe",port=4444)
+    #had issue getting this path to to work, you may have trouble as well
+    driver = webdriver.Chrome("chromedriver.exe")
     driver.get("http://www.xkcd.org")
     return driver
 def seleniumTest():
     driver = setup()
-    python_link = driver.find_elements_by_xpath("//*[@id='middleContainer']/ul[1]/li[2]/a")[0]
-    archive_link = "/html/body/div[1]/div[1]/ul/li[1]/a"
-    thirdArchivedComic = "/html/body/div[2]/a[3]"
-    python_link.click()
-    time.sleep(5)
-    python_link.click()
-    time.sleep(5)
-    pageTitle = driver.title
-    archive_link.click()
-    time.sleep(5)
+    backBtn = driver.find_elements_by_xpath("//*[@id='middleContainer']/ul[1]/li[2]/a")[0]
+    archive_link = driver.find_elements_by_xpath("/html/body/div[1]/div[1]/ul/li[1]/a")[0]
+    thirdArchivedComic = driver.find_elements_by_xpath("/html/body/div[2]/a[3]")[0]
+    backBtn.click()
+    time.sleep(sleepTime)
+    backBtn.click()
+    time.sleep(sleepTime)
+    pageTitleOfThirdComic = driver.title
+    backBtn.click()
+    time.sleep(sleepTime)
     thirdArchivedComicText = thirdArchivedComic.text
-    assert (pageTitle == thirdArchivedComicText, "the titles don't match") 
-    return "test is over"
-    
+    assert (pageTitleOfThirdComic == thirdArchivedComicText, "the titles don't match") 
+    exit()
+    return "Test Passed and closed without issue"
+def exit():
+    driver.close()
+    return driver
 seleniumTest()
